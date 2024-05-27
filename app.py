ca9a284909c15082b562d1182d3a75e1b0e315cd
niltonsainz <161954152+niltonsainz@users.noreply.github.com>
2024-05-27 03:13:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ st.markdown(
         height: auto;
     }
     </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     """,
     unsafe_allow_html=True
 )
