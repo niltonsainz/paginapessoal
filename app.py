2d1797ce8f789643b2eb591840077bae51455a97
niltonsainz <161954152+niltonsainz@users.noreply.github.com>
2024-05-27 00:29:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -62,9 +62,26 @@ selected = option_menu(
     }
 )
 
-# Conteúdo das abas
 if selected == "Apresentação":
-    st.write("Bem-vindo à página de apresentação!")
+    st.markdown("""
+    Me chamo Nilton Garcia Sainz, tenho 27 anos, nascido em Rio Grande, no extremo sul do Rio Grande do Sul.
+
+    Atualmente, faço doutorado em Ciência Política no Programa de Pós-Graduação em Ciência Política da Universidade
+    Federal do Paraná, onde também sou pós-graduando em Data Science & Big Data, curso oferecido pelos Departamentos
+    de Estatística e Informática da UFPR. Realizo graduação em Direito na Pontifícia Universidade Católica do Paraná,
+    onde também atuo como pesquisador. Sou graduado em Ciências Sociais e mestre em Ciência Política pela Universidade
+    Federal de Pelotas.
+
+    Sou pesquisador vinculado ao [Instituto Nacional de Ciência Tecnologia - Representação e Legitimidade Democrática (INCT-ReDem)](https://redem.tec.br/)
+    e ao [Núcleo de Pesquisa em Políticas Públicas e Desenvolvimento Humano da PUCPR (NUPED)](https://www.nuped.com.br/).
+    
+    Meus interesses de pesquisa são elites parlamentares, carreiras políticas, eleições e poder Legislativo 
+    e metodologia de pesquisa em Ciência Política e Sociais. Também possuo interesse nas áreas de regulação de novas técnicas
+    e transformação da administração pública digital.
+
+    Atuo como pesquisador e analista de dados no Observatório do Controle, onde atualmente estou como Diretor de pesquisa. Também presto
+    assessoria e consultoria em pesquisas científicas.
+    """)
 elif selected == "Projetos":
     st.write("Aqui estão os meus projetos:")
     projects = [
